65f06ca5a6e42299cdcc69a74c7248331d41b8a5
Shorif <shorifahamed571@gmail.com>
2023-12-22 03:38:00 +0600

    Project Setup with MongoDBt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,13 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-//crewtasker
-
-//r8JXvvWbvzbJtVIH
-
-
-
-const uri = "mongodb+srv://<username>:<password>@cluster0.shfwl8n.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.shfwl8n.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
